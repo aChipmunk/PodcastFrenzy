3770096046df7ae2880b40071aa4d6c3dc7d801a
Max Caplow <maxcaplow@gmail.com>
2013-06-24 18:09:55 -0700

    Add feed now works! Refresh does stuff!  Currently add feed only sets the url to the var "feedUrl1" this will later be a database. Refresh call getView(feedUrl) this is sort of good, but will need to updated as everything moves to databases!

--- PodcastMan/src/com/achipmunkdev/podcasts/MainActivity.java
+++ PodcastMan/src/com/achipmunkdev/podcasts/MainActivity.java
@@ -6,9 +6,20 @@ import java.net.URL;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import com.achipmunkdev.podcasts.R;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.DialogInterface;
 
 import com.achipmunkdev.podcasts.FeedListAdapter;
 import com.google.code.rome.android.repackaged.com.sun.syndication.feed.synd.SyndFeed;
@@ -19,10 +30,12 @@ import com.google.code.rome.android.repackaged.com.sun.syndication.io.FeedExcept
 
 public class MainActivity extends Activity {
 	
+	EditText urlEditText = null;
+	private static final int DIALOG_ALERT = 1;
 	public FeedListAdapter feedToListAdapter;
 	public ListView mainListView;
 	public SyndFeed aFeed;
-	public final String feedUrl1 = "http://feeds.wnyc.org/radiolab?format=xml";
+	public String feedUrl1 = "http://feeds.wnyc.org/radiolab?format=xml";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +57,21 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	switch (item.getItemId()){
+    	case R.id.action_add_feed:
+    		showDialog(DIALOG_ALERT);
+    		//startActivity(new Intent(this, AddFeedActivity.class));
+    		
+    		//startActivity(new Intent(this, AddFeedActivity.class));
+    		return true;
+    	case R.id.action_refresh:
+    		getView(MainActivity.this, feedUrl1);
+    		
+    	}
+    	return false;
     }
     
     public void getView(final Activity activity, String url){
@@ -78,5 +106,42 @@ public class MainActivity extends Activity {
 	        FeedFetcher feedFetcher = new HttpURLFeedFetcher();
 	        return feedFetcher.retrieveFeed( new URL( feedUrl ) );
 	    }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+    	//commented code allows for multiple dialogues to be created! YAY!
+    	//switch(id){
+    	//case DIALOG_ALERT:
+    		Builder builder = new AlertDialog.Builder(this);
+    		LayoutInflater inflater = this.getLayoutInflater();
+    		View v = inflater.inflate(R.layout.add_feed_layout, null);
+    		urlEditText = (EditText)v.findViewById(R.id.feed_input_field);
+    		
+    		builder.setView(v);
+    		builder.setPositiveButton(R.string.submit_button_text, new DialogInterface.OnClickListener(){
+    				@Override
+    				public void onClick(DialogInterface dialog, int id){
+    					feedUrl1 = urlEditText.getText().toString();
+    					urlEditText.setText("");
+    					dialog.dismiss();
+    					
+    				}
+    				
+    			});
+    		builder.setNegativeButton(R.string.cancle_button_text, new DialogInterface.OnClickListener() {
+    				
+					
+					@Override
+					public void onClick(DialogInterface dialog, int id) {
+						urlEditText.setText("");
+						dialog.dismiss();
+						
+					}
+				});
+    		return builder.create();
+    		
+    	
+    	//}
+    	
+    }
     
 }
